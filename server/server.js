e2f18c0b524003566d48071f8009733ae8e098ec
Galay Yuriy <y4109galay@gmail.com>
2019-12-31 23:20:03 +0200

    remove router on server-side

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
-const router = require('./router/router');
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 const PORT = process.env.PORT || 8000;
@@ -32,7 +31,6 @@ io.on('connection', (socket) => {
        const user = getUser(socket.id);
 
        io.to(user.room).emit('message', { user: user.name, text: message });
-       io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
 
        callback();
    }); 
@@ -41,12 +39,11 @@ io.on('connection', (socket) => {
        const user = removeUser(socket.id);
 
        if(user){
-           io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left.`})
+           io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left.`});
+           io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
        }
    });
 });
-
-app.use(router);
 
 server.listen(PORT, () => {
     console.log(`Server has start on port ${PORT}`);
